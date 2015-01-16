92213ccb3658e414e3bd498f6a96eb6d4731e181
brunomewius <brunomewius@gmail.com>
2015-01-16 21:58:41 -0200

    Criação Bruno Cleodir

---
+++ pages/fornecedor.php
@@ -0,0 +1,100 @@
+	<h1>Prestador de Serviços</h1>
+
+	<p>Explicação do cadastro</p>
+
+
+		<form name="frmCadastraVeiculo" method="POST" action="">
+
+			<table class="table">
+				<tr>
+					<td align=right>*Nome</td>
+					<td><input type="text" name="nome" id="nome" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Telefone</td>
+					<td><input type="text" name="telefone" id="telefone" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Celular</td>
+					<td><input type="text" name="celular" id="celular" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>E-mail <small>(opcional)</small></td>
+					<td><input type="text" name="email" id="email" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Site</td>
+					<td><input type="text" name="site" id="site" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*CEP</td>
+					<td><input type="text" name="cep" id="cep" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Logradouro</td>
+					<td><input type="text" name="logradouro" id="logradouro" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Numero</td>
+					<td><input type="text" name="nº" id="nº" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Bairro</td>
+					<td><input type="text" name="bairro" id="bairro" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Cidade</td>
+					<td><input type="text" name="cidade" id="cidade" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Estado</td>
+					<td><input type="text" name="estado" id="estado" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Latitude</td>
+					<td><input type="text" name="latitude" id="latitude" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Longitude</td>
+					<td><input type="text" name="longitude" id="longitude" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*CPF/CNPJ</td>
+					<td><input type="text" name="cpf_cnpj" id="cpf_cnpj" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Ie</td>
+					<td><input type="text" name="ie" id="ie" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Serviços</td>
+					<td><input type="text" name="servicos" id="servicos" value="" /></td>
+				</tr>
+
+				<tr>
+					<td align=right>*Login</td>
+					<td><input type="text" name="login" id="login" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Senha</td>
+					<td><input type="password" name="senha" id="senha" value="" /></td>
+				</tr>
+				<tr>
+					<td align=right>*Confirmar Senha</td>
+					<td><input type="password" name="confirmaSenha" id="confirmaSenha" value="" /></td>
+				</tr>
+				<tr>
+					<td colspan=2 align=center>
+	<input type='checkbox' name='concordaTermos' id='concordaTermos' value='Sim' /> 
+	* Concordo com os Termos de Serviço e a Política de Privacidade.
+					</td>
+				</tr>						
+				<tr>
+
+					<td colspan=2 align=center><input type="submit"  name="cadastrar" value="Cadastrar"/></td>
+				</tr>
+			</table>
+
+		</form>
+</div>	
+
