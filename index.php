166f5ab37b8d03270eabee4b74187d3fbf4f9dde
richard1983 <rllsrichard@gmail.com>
2015-01-22 21:50:42 -0200

    meus dados configurando page

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if ($_GET['sair'] == "true") {
     <script src="bootstrap/dist/js/bootstrap.js"></script>
 
 
-    <center><h1>Agende-se</h1></center>
+    <center><h1>Academia da Web</h1></center>
 
     <nav class="navbar navbar-inverse" role="navigation">
       <div class="container-fluid">
@@ -49,7 +49,7 @@ if ($_GET['sair'] == "true") {
 
 <?php
 if ($_SESSION['logado']) { ?>          
-          <a class="navbar-brand" href="http://localhost/agendese/?sair=true">Meus Dados</a>
+          <a class="navbar-brand" href="http://localhost/agendese/?page=meusdados">Meus Dados</a>
           <a class="navbar-brand" href="http://localhost/agendese/?sair=true">Sair</a>
 
 <?php } ?>
@@ -66,7 +66,7 @@ if (!$_SESSION['logado']) { ?>
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Cadastre-se<span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
                 <li><a href="http://localhost/agendese/?page=cadastre-se">Cliente</a></li>
-                <li><a href="http://localhost/agendese/?page=cadastre-se">Prestador de ServiÁos</a></li>
+                <li><a href="http://localhost/agendese/?page=cadastre-se">Prestador de Servi√ßos</a></li>
               </ul>
             </li>
           </ul>
@@ -83,12 +83,12 @@ if (!$_SESSION['logado']) { ?>
 if ($_SESSION['tipo'] == "Administrador") { ?>
           <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">RelatÛrios <span class="caret"></span></a>
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Relat√≥rios <span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
                 <li><a href="#">Geral</a></li>
-                <li><a href="#">Anivers·rio</a></li>
+                <li><a href="#">Anivers√°rio</a></li>
                 <li><a href="#">Telefones</a></li>
-                <li><a href="#">EndereÁos</a></li>
+                <li><a href="#">Endere√ßos</a></li>
               </ul>
             </li>
           </ul>
@@ -103,12 +103,12 @@ if ($_SESSION['tipo'] == "Administrador") { ?>
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" id="exampleModalLabel">AutenticaÁ„o</h4>
+        <h4 class="modal-title" id="exampleModalLabel">Autentica√ß√£o</h4>
       </div>
       <div class="modal-body">
         <form role="form">
           <div class="form-group">
-            <label for="recipient-name" class="control-label">Usu·rio:</label>
+            <label for="recipient-name" class="control-label">Usu√°rio:</label>
             <input type="text" class="form-control" id="ds_usuario" name="ds_usuario">
           </div>
           <div class="form-group">
@@ -138,13 +138,13 @@ if ($_SESSION['tipo'] == "Administrador") { ?>
   </div>
 <?php
 
-if ($_GET['page'] == "cadastre-se") {
-  require("pages/cadastre-se.php");
+if ($_GET['page'] != "") {
+  require("pages/".$_GET['page'].".php");
 } else {
 
   if ($_SESSION['logado']) {
-    echo "Ol· ".$_SESSION['nomeCliente'];
-    echo "<br /> Seu acesso È de ".$_SESSION['tipo'].".";
+    echo "Ol√° ".$_SESSION['nomeCliente'];
+    echo "<br /> Seu acesso √© de ".$_SESSION['tipo'].".";
   }
 }
 
