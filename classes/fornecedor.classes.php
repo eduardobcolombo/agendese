2d821181966013e48517436421a9e9448eef1117
brunomewius <brunomewius@gmail.com>
2015-01-24 15:17:08 -0200

    Arrumando codigo do fornecedor

--- classes/fornecedor.classes.php
+++ classes/fornecedor.classes.php
@@ -21,136 +21,41 @@ class Fornecedor
 	private $senha;
 	private $tipo;
 
+	/* OS SETS*/
 	function setNome($nome) {$this ->nome = $nome;}
 	function setTelefonevate($telefone) {$this ->telefone = $telefone;}
+	function setCelular($celular) {$this ->celular = $celular;}	
+	function setEmail($email) {$this ->email = $email;}	
+	function setSite($site) {$this ->site = $site;}
+	function setCep($cep) {$this ->cep = $cep;}	
+	function setLogradouro($logradouro) {$this ->logradouro = $logradouro;}
+	function setNumero($n∫) {$this ->n∫ = $n∫;}
+	function setBairro($bairro) {$this ->bairro = $bairro;}
+	function setCidade($cidade) {$this ->cidade = $cidade;}
+	function setEstado($estado) {$this ->estado = $estado;}
+	function setLatitude($latitude) {$this ->latitude = $latitude;}
+	function setLongitude($longitude) {$this ->longitude = $longitude;}
+	function setCpf_cnpj($cpf_cnpj) {$this ->cpf_cnpj = $cpf_cnpj;}
+	function setIe($ie) {$this ->ie = $ie;}
+	function setServicos($servicos) {$this ->servicos = $servicos;}
 
-	function setCelular($celular) {$this ->celular = $celular;}
-	
-	
-	function setEmail($email) {	$this ->email = $email;	}	
-
-	function setSite($site) 
-	{
-		$this ->site = $site;
-			}
-	function setCep($cep) 
-	{
-		$this ->cep = $cep;
-			}	
-	function setLogradouro($logradouro) 
-	{
-		$this ->logradouro = $logradouro;
-			}
-	function setNumero($n¬∫) 
-	{
-		$this ->n¬∫ = $n¬∫;
-			}
-	function setBairro($bairro) 
-	{
-		$this ->bairro = $bairro;
-			}
-	function setCidade($cidade) 
-	{
-		$this ->cidade = $cidade;
-			}
-	function setEstado($estado) 
-	{
-		$this ->estado = $estado;
-			}
-	function setLatitude($latitude) 
-	{
-		$this ->latitude = $latitude;
-	}
-	function setLongitude($longitude) 
-	{
-		$this ->longitude = $longitude;
-			}
-	function setCpf_cnpj($cpf_cnpj) 
-	{
-		$this ->cpf_cnpj = $cpf_cnpj;
-			}
-	function setIe($ie) 
-	{
-		$this ->ie = $ie;
-			}
-	function setServicos($servicos) 
-	{
-		$this ->servicos = $servicos;
-			}
-
-	function getNome($nome) 
-	{
-		return $this ->nome;
-		}
-	function getTelefone($telefone) 
-	{
-		return $this ->telefone;
-		}
-
-	function getCelular($celular) 
-	{
-		return $this ->celular;
-	
-	function getEmail($email) 
-	{
-		return $this ->email;
-			}	
-	function getSite($site) 
-	{
-		return $this ->site;
-			}
-	function getCep($cep) 
-	{
-		return $this ->cep;
-			}	
-	function getLogradouro($logradouro) 
-	{
-		return $this ->logradouro;
-			}
-	function getNumero($n¬∫) 
-	{
-		return $this ->n¬∫;
-			}
-	function getBairro($bairro) 
-	{
-		return $this ->bairro;
-			}
-	function getCidade($cidade) 
-	{
-		return $this ->cidade;
-			}
-	function getEstado($estado) 
-	{
-		return $this ->estado;
-			}
-	function getLatitude($latitude) 
-	{
-		return $this ->latitude;
-			}
-	function getLongitude($longitude) 
-	{
-		return $this ->longitude;
-			}
-	function getCpf_cnpj($cpf_cnpj) 
-	{
-		return $this ->cpf_cnpj;
-			}
-	function getIe($ie) 
-	{
-		return $this ->ie;
-			}
-	function getServicos($servicos) 
-	{
-		return $this ->servicos;
-			}
-
-
-
-
-
-
-
-
+	/* OS GETS*/
+	function getNome($nome) {return $this ->nome;}
+	function getTelefone($telefone) {return $this ->telefone;}
+	function getCelular($celular) {return $this ->celular;}
+	function getEmail($email) {return $this ->email;}
+	function getSite($site) {return $this ->site;}
+	function getCep($cep) {return $this ->cep;}
+	function getLogradouro($logradouro) {return $this ->logradouro;}
+	function getNumero($n∫) {return $this ->n∫;}
+	function getBairro($bairro) }return $this ->bairro;}
+	function getCidade($cidade) {return $this ->cidade;}
+	function getEstado($estado) {return $this ->estado;}
+	function getLatitude($latitude) {return $this ->latitude;}
+	function getLongitude($longitude) {return $this ->longitude;}
+	function getCpf_cnpj($cpf_cnpj) {return $this ->cpf_cnpj;}
+	function getIe($ie) {return $this ->ie;}
+	function getServicos($servicos) {return $this ->servicos;}
 
 
 	function setLogin($login) 
@@ -183,7 +88,7 @@ class Fornecedor
 		return $this ->tipo;
 			}		
 
- // FUN√á√ÉO INSERIR NO BANCO DE DADOS  
+ // FUN«á√ÉO INSERIR NO BANCO DE DADOS  
 
 	function setDB($id_fornecedor = null) {
 			GLOBAL $db;
