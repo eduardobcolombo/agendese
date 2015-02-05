d412787bc7e546de958e73e2f75042d6f8542608
richard1983 <rllsrichard@gmail.com>
2015-02-05 21:20:13 -0200

    pesquisa fornecedor

--- pages/pesquisa.php
+++ pages/pesquisa.php
@@ -1,13 +1,30 @@
 <?php
-var_dump($_POST);
-$pesquisa = $objCliente->getClientePesquisa($_POST['filtro']);
+
+$pesquisa = $objFornecedor->getFornecedorPesquisa($_POST['filtro']);
+	
 
 	while($linha = $pesquisa->fetch(PDO::FETCH_OBJ)) {
-		echo $linha->nomeCliente;
-		echo "<br />";
-	}
+		echo  $linha->nome;
+		echo " - ";
+		echo $linha->servicos;
+		echo " - ";
+		echo $linha->telefone;
+		echo " - ";
+		echo $linha->celular;
+		echo " - ";
+		echo $linha->site;
+		echo " - ";
+		echo $linha->logradouro;
+		echo " - ";
+		echo $linha->bairro;
+		echo " - ";
+		echo $linha->cidade;
+		echo " - ";
+		echo $linha->estado;
+		echo " - ";
 
-
+	} 
 
 ?>
+
 
