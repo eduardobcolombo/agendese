838d40cad3f5b07439554fef65da78b66e1a4993
brunomewius <brunomewius@gmail.com>
2015-02-06 21:35:47 -0200

    atualização

--- pages/contato.php
+++ pages/contato.php
@@ -1,9 +1,9 @@
-<h1>Contato</h1>
 <div id="boxContato">
+<h1>Contato</h1>
 	<br />Nome: <input type="text" name="nome" />
 	<br />Email:<input type="text" name="email" />
 	<br />Telefone:<input type="text" name="fone" />
-	<br />Mensagem: <textarea rows="10" cols="50"></textarea>
+	<br />Mensagem: <textarea rows="10" cols="45"></textarea>
 	<br /> <input type="submit" name="btnOk" value="Enviar" />
 </div>
 
